e21f6a816461e8c676b6a5bd7b2684a41a29bc9b
Limbio <limbio32@gmail.com>
2026-06-05 15:07:53 +0400

    Initial commit: full project ready for deploy

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -28,17 +28,53 @@ async function loadDishes() {
 
     const cardsContainer = document.querySelector('.cards');
     const card = document.querySelector('.card');
+    
 
     dishes.forEach(dish => {
         const new_card = card.cloneNode(true);
         new_card.querySelector('.card__dishname p').textContent = dish.name;
-        new_card.querySelector('.card__price').textContent = Number(dish.price).toFixed(2) + " ₾";
-        new_card.querySelector('.card__photo img').src = dish.mainImageUrl;
-
-
+        new_card.querySelector('.card__price span').textContent = Number(dish.price).toFixed(2);
+        //new_card.querySelector('.card__photo img').src = dish.mainImageUrl;
+        new_card.querySelector('.card__photo').style.backgroundImage = `url('${dish.mainImageUrl}')`;
         cardsContainer.appendChild(new_card);
+        
+        const in_cart_button = new_card.querySelector('.card__in-cart-button');
+        const minus_button = new_card.querySelector('.card__minus-button');
+        const plus_button = new_card.querySelector('.card__plus-button');
+        const quantity_input = new_card.querySelector('.card__quantity-input');
+        const price = new_card.querySelector('.card__price span');
+        const total_price = new_card.querySelector('.card__total-price span');
+
+        minus_button.addEventListener("click", () => {
+            if(quantity_input.value >= 1){
+                quantity_input.value--;
+                total_price.textContent = (Number(price.textContent) * Number(quantity_input.value)).toFixed(2);
+            }
+            if(quantity_input.value <= 0){
+                in_cart_button.classList.remove("card__in-cart-button_green");
+            }
+        });
+        plus_button.addEventListener("click", () => {
+            quantity_input.value++;
+             if(quantity_input.value > 0){
+                in_cart_button.classList.add("card__in-cart-button_green");
+            }
+            total_price.textContent = (Number(price.textContent) * Number(quantity_input.value)).toFixed(2);
+        });
+
+        quantity_input.addEventListener("input", () => {
+            if (quantity_input.value <= 0) {
+                quantity_input.value = 0;
+                in_cart_button.classList.remove("card__in-cart-button_green");
+            }
+            else if(quantity_input.value > 0){
+                in_cart_button.classList.add("card__in-cart-button_green");
+            }
+            total_price.textContent = (Number(price.textContent) * Number(quantity_input.value)).toFixed(2);
+        });
+
     });
+    card.style.display = "none";
 }
 loadDishes();
-
 
